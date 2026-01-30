3fb010ef588f5909d79fa2be4c53d6f6e2edeccc
samb1243 <sb1629@exeter.ac.uk>
2026-01-30 12:04:07 +0000

    NumbersInSpanish completed with switch-case

--- NumbersInSpanish.java
+++ NumbersInSpanish.java
@@ -3,23 +3,25 @@ public class NumbersInSpanish {
        // read the first argument passed to the program
        int number = Integer.parseInt(args[0]); 
 
-       if (number == 1){
-        System.out.println("uno");
-       }
-       else if (number == 2){
-        System.out.println("dos");
-       }
-       else if(number == 3){
-        System.out.println("tres");
-       }
-       else if(number == 4){
-        System.out.println("cuatro");
-       }
-       else if(number == 5){
-        System.out.println("cinco");
-       }
-       else{
-        System.out.println("Sorry I do not know that");
+       switch (number) {
+        case 1:
+            System.out.println("uno");
+            break;
+        case 2:
+            System.out.println("dos");
+            break;
+        case 3:
+            System.out.println("tres");
+            break;
+        case 4:
+            System.out.println("cuatro");
+            break;
+        case 5:
+            System.out.println("cinco");
+            break;
+       
+        default:
+            break;
        }
  
        // delete the line below and replace with your code
